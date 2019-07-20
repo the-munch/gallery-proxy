6fcaa953292b3940d3700eafd2032ce9d508f350
Tina Le <tinale09@gmail.com>
2019-07-20 12:34:26 -0700

    munch logo

--- server/index.js
+++ server/index.js
@@ -1,19 +1,18 @@
 const express = require ('express');
-const PORT = 5001;
+const PORT = 5004;
 const app = express();
 const path = require('path'); 
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer();
 
 app.use(morgan('dev'));
 app.use('/:id', express.static(path.join(__dirname, '/../client')));
 
-const gallery = 'http://localhost:3000';
-const reservation = 'http://localhost:3001';
-const popular = 'http://localhost:3002';
-const header = 'http://localhost:3003';
+const gallery = 'http://ec2-52-53-207-161.us-west-1.compute.amazonaws.com:3000';
+const reservation = 'http://ec2-18-217-9-12.us-east-2.compute.amazonaws.com:3001';
+const popular = 'http://ec2-3-95-234-77.compute-1.amazonaws.com';
+const header = 'http://ec2-18-223-24-238.us-east-2.compute.amazonaws.com:3003/';
 
 app.all('/gallery/:id', function(req, res) {
     console.log('redirecting to gallery');
